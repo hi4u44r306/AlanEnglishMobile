7213ecb03fb369f91d9c7f6a12bc8a3d61fda821
Victor <hi4u44r306@gmail.com>
2022-11-11 23:13:23 +0800

    Test

--- screens/Login.js
+++ screens/Login.js
@@ -5,91 +5,102 @@ import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView, Text, View, TextInput } from "react-native";
 import { Brand, SubBrand, FocusedStatusBar, LoginButton, Blackboard, Copyright } from "../components";
 import { COLORS, SIZES, assets, FONTS } from "../constants";
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { KeyboardAvoidingView } from 'react-native-web';
 
 
 const Login = () => {
     const navigation = useNavigation();
   return (
-    <SafeAreaView style={{backgroundColor:COLORS.ricewhite, width:"100%", height:"100%", paddingTop:"30px" }}>
+    <SafeAreaView style={{backgroundColor:COLORS.ricewhite, width:"100%", height:"100%", }}>
         <FocusedStatusBar backgroundColor={COLORS.primary} />
-        <View style={{ marginTop: SIZES.font, alignItems: "center", justifyContent:"center" }}>
-            <Blackboard/>
-            <Brand/>
-            <SubBrand/>
-            <View
-            style={{
-                fontFamily: FONTS.VarelaRound,
-                width: "90%",
-                borderRadius: SIZES.font,
-                flexDirection: "column",
-                alignItems: "center",
-                margin: 20,
-                paddingHorizontal: SIZES.font,
-                paddingVertical: SIZES.small - 2,
-                borderColor: "#ffbf3f",
-                borderWidth: 10,
-                borderRadius:10,
-            }}
-            >
-                <View style={{
-                    width: "100%",
+        <TouchableWithoutFeedback
+        onPress={Keyboard.dismiss}
+        >
+        <KeyboardAvoidingView 
+            behavior="padding" 
+            // style={styles.container}
+        >
+            <View style={{ marginTop: SIZES.font, alignItems: "center", justifyContent:"center" }}>
+                <Blackboard/>
+                <Brand/>
+                <SubBrand/>
+                <View
+                style={{
+                    fontFamily: FONTS.VarelaRound,
+                    width: "90%",
+                    borderRadius: SIZES.font,
+                    flexDirection: "column",
                     alignItems: "center",
-                }}>
-                    <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>帳號</Text>
-                    <TextInput
-                        placeholder="Email..."
-                        style={{ 
-                            fontFamily: FONTS.VarelaRound,
-                            outline:"none",
-                            flex: 1,
-                            fontSize:20,
-                            textAlign: 'center',
-                            justifyContent: 'center',
-                            color: COLORS.primary, 
-                            borderColor: COLORS.inputfieldgreen,
-                            borderWidth: 3.5,
-                            borderRadius:5,
-                            padding:SIZES.small,
-                            margin:10,
-                            width: "80%",
+                    margin: 20,
+                    paddingHorizontal: SIZES.font,
+                    paddingVertical: SIZES.small - 2,
+                    borderColor: "#ffbf3f",
+                    borderWidth: 10,
+                    borderRadius:10,
+                }}
+                >
+                    <View style={{
+                        width: "100%",
+                        alignItems: "center",
+                    }}>
+                        <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>帳號</Text>
+                        <TextInput
+                            placeholder="Email..."
+                            style={{ 
+                                fontFamily: FONTS.VarelaRound,
+                                outline:"none",
+                                flex: 1,
+                                fontSize:20,
+                                textAlign: 'center',
+                                justifyContent: 'center',
+                                color: COLORS.primary, 
+                                borderColor: COLORS.inputfieldgreen,
+                                borderWidth: 3.5,
+                                borderRadius:5,
+                                padding:SIZES.small,
+                                margin:10,
+                                width: "80%",
+                                }}
+                        />
+                    </View>
+                    <View style={{
+                        width: "100%",
+                        flexDirection:"column",
+                        alignItems: "center",
+                    }}>
+                        <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>密碼</Text>
+                        <TextInput
+                            placeholder="Password..."
+                            style={{ 
+                                fontFamily: FONTS.VarelaRound,
+                                outline:"none",
+                                flex: 1,
+                                fontSize:20,
+                                textAlign: 'center',
+                                justifyContent: 'center',
+                                color: COLORS.primary, 
+                                borderColor: COLORS.inputfieldgreen,
+                                borderWidth: 3.5,
+                                borderRadius: 5,
+                                padding:SIZES.small,
+                                margin:10,
+                                width: "80%",
                             }}
+                            // onChangeText={onSearch}
+                        />
+                    </View>
+                    <LoginButton
+                        margin={20}
+                        minWidth={120}
+                        fontSize={SIZES.extraLarge}
+                        handlePress={() => navigation.navigate("Home")}
                     />
+                    <Copyright/>
                 </View>
-                <View style={{
-                    width: "100%",
-                    flexDirection:"column",
-                    alignItems: "center",
-                }}>
-                    <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>密碼</Text>
-                    <TextInput
-                        placeholder="Password..."
-                        style={{ 
-                            fontFamily: FONTS.VarelaRound,
-                            outline:"none",
-                            flex: 1,
-                            fontSize:20,
-                            textAlign: 'center',
-                            justifyContent: 'center',
-                            color: COLORS.primary, 
-                            borderColor: COLORS.inputfieldgreen,
-                            borderWidth: 3.5,
-                            borderRadius: 5,
-                            padding:SIZES.small,
-                            margin:10,
-                            width: "80%",
-                        }}
-                        // onChangeText={onSearch}
-                    />
-                </View>
-                <LoginButton
-                    margin={20}
-                    minWidth={120}
-                    fontSize={SIZES.extraLarge}
-                    handlePress={() => navigation.navigate("Home")}
-                />
-                <Copyright/>
             </View>
-        </View>
+        </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     </SafeAreaView>
   )
 }
