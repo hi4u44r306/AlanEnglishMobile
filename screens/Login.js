29162612342679459a5c51e2367c6db21b82fd8b
Victor <hi4u44r306@gmail.com>
2022-11-12 23:24:52 +0800

    test

--- screens/Login.js
+++ screens/Login.js
@@ -2,7 +2,7 @@
 // import { View, SafeAreaView, FlatList } from "react-native";
 import React from 'react'
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView, Text, View, TextInput, StyleSheet } from "react-native";
+import { SafeAreaView, Text, View, TextInput, StyleSheet,Keyboard,KeyboardAvoidingView, TouchableWithoutFeedback,Platform} from "react-native";
 import { Brand, SubBrand, FocusedStatusBar, LoginButton, Blackboard, Copyright } from "../components";
 import { COLORS, SIZES, assets, FONTS } from "../constants";
 
@@ -13,67 +13,73 @@ const Login = () => {
     const [password, onChangePassword] = React.useState("");
 
     return (
-        <SafeAreaView style={{backgroundColor:COLORS.ricewhite, width:"100%", height:"100%", }}>
-        <FocusedStatusBar backgroundColor={COLORS.primary} />
-        
-
-            <View style={{ marginTop: SIZES.font, alignItems: "center", justifyContent:"center" }}>
-            
-                <Blackboard/>
-                <Brand/>
-                <SubBrand/>
-                <View
-                style={{
-                    fontFamily: FONTS.bold,
-                    width: "90%",
-                    borderRadius: SIZES.font,
-                    flexDirection: "column",
-                    alignItems: "center",
-                    margin: 20,
-                    paddingHorizontal: SIZES.font,
-                    paddingVertical: SIZES.small - 2,
-                    borderColor: "#ffbf3f",
-                    borderWidth: 10,
-                    borderRadius:10,
-                }}
-                >
-                    
-                <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>帳號</Text>
-                <TextInput
-                    placeholder="Email..."
-                    value={email}
-                    onChangeText={onChangeEmail}
-                    style={styles.input}
-                />
-            
-                <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>密碼</Text>
-                <TextInput
-                    placeholder="Password..."
-                    value={password}
-                    onChangeText={onChangePassword}
-                    style={styles.input}
-                />
-                    <LoginButton
-                        margin={20}
-                        minWidth={120}
-                        fontSize={SIZES.extraLarge}
-                        handlePress={() => navigation.navigate("Home")}
+        // <SafeAreaView style={{backgroundColor:COLORS.ricewhite, width:"100%", height:"100%", }}>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            {/* <FocusedStatusBar backgroundColor={COLORS.primary} /> */}
+                <View style={{ marginTop: SIZES.font, alignItems: "center", justifyContent:"center" }}>
+                
+                    <Blackboard/>
+                    <Brand/>
+                    <SubBrand/>
+                    <View
+                    style={{
+                        fontFamily: FONTS.bold,
+                        width: "90%",
+                        borderRadius: SIZES.font,
+                        flexDirection: "column",
+                        alignItems: "center",
+                        margin: 20,
+                        paddingHorizontal: SIZES.font,
+                        paddingVertical: SIZES.small - 2,
+                        borderColor: "#ffbf3f",
+                        borderWidth: 10,
+                        borderRadius:10,
+                    }}
+                    >
+                        
+                    <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>帳號</Text>
+                    <TextInput
+                        placeholder="Email..."
+                        value={email}
+                        onChangeText={onChangeEmail}
+                        style={styles.input}
                     />
-                    <Copyright/>
+                
+                    <Text style={{fontSize: SIZES.extraLarge,fontWeight:900,margin:10}}>密碼</Text>
+                    <TextInput
+                        placeholder="Password..."
+                        value={password}
+                        onChangeText={onChangePassword}
+                        style={styles.input}
+                    />
+                        <LoginButton
+                            margin={20}
+                            minWidth={120}
+                            fontSize={SIZES.extraLarge}
+                            handlePress={() => navigation.navigate("Home")}
+                        />
+                        <Copyright/>
+                    </View>
                 </View>
-            </View>
-
-    </SafeAreaView>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    // </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
     input: {
         fontFamily: FONTS.semiBold,
         outline:"none",
         fontSize:18,
         textAlign: 'center',
         justifyContent: 'center',
-        // backgroundColor: "#bbdef0",
         backgroundColor: "white",
         color: COLORS.primary, 
         borderColor: COLORS.inputfieldgreen,
